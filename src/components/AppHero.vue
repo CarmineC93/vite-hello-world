f32c3d1780b67b58095160fe4f2cb3b2304e3b75
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2022-11-07 17:51:11 +0100

    m

---
+++ src/components/AppHero.vue
@@ -0,0 +1,23 @@
+<script>
+export  {
+    data() {
+        return {
+            title: "La mia prima app con Vite!"
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="jumbo">{{title}}</div>
+</template>
+
+<style scoped>
+
+.jumbo{
+    width: 100%;
+    height: 200px;
+    background-color: beige;
+}
+
+</style>
